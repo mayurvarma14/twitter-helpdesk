87007144cc4999bd4cc124ec6d0e763038bd1e15
Mayur Varma <mayurvarma14@gmail.com>
2020-10-10 23:06:12 +0530

    chore:update auth webhook auth to use user creds

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const passport = require('passport');
 const { validateWebhook, validateSignature } = require('twitter-autohook');
+
+const { decrypt } = require('../utils/crypto');
 const router = express.Router();
 
 router.get('/twitter', passport.authenticate('twitter'));
@@ -19,8 +21,8 @@ router.get('/twitter/webhook', function(req, res, next) {
     console.log('Log: req.query.crc_token', req.query.crc_token);
 
     const auth = {
-      token: process.env.TWITTER_ACCESS_TOKEN,
-      token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+      token: decrypt(req.user.token),
+      token_secret: decrypt(req.user.tokenSecret),
       consumer_key: process.env.TWITTER_CONSUMER_KEY,
       consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
       env: process.env.TWITTER_WEBHOOK_ENV,
