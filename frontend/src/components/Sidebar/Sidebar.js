88d55a57333f3daa77f07d42892b3295e032ddde
Mayur Varma <mayurvarma14@gmail.com>
2020-10-11 18:17:06 +0530

    feat:add sidebar

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 function Sidebar() {
-  return <aside className="sidebar"></aside>;
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-top">
+        <img src="/favicon.png" alt="" />
+      </div>
+      <div className="sidebar-center">
+        <i class="far fa-clock"></i>
+        <i class="fas fa-home"></i>
+        <i class="fas fa-user-friends"></i>
+        <i class="far fa-comments active"></i>
+        <i class="far fa-credit-card"></i>
+        <i class="fas fa-store"></i>
+      </div>
+      <div className="sidebar-bottom">
+        <i class="far fa-life-ring"></i>
+        <img src="/profile.jpg" alt="profile image2" className="profile" />
+      </div>
+    </aside>
+  );
 }
 
 export default Sidebar;
