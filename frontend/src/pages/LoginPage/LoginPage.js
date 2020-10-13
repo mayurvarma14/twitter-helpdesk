4fc29685fe52bc665f332cebf37f2053798cc8c2
Mayur Varma <mayurvarma14@gmail.com>
2020-10-13 09:40:21 +0530

    chore:add login button

--- frontend/src/pages/LoginPage/LoginPage.js
+++ frontend/src/pages/LoginPage/LoginPage.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-
+import './LoginPage.scss';
 class LoginPage extends Component {
   render() {
     return (
-      <div>
-        <a href={`${process.env.REACT_APP_BACKEND_URL}/auth/twitter`}>
-          Login with twitter
-        </a>
+      <div className="login-page">
+        <div className="login-title">Welcome to Twitter Helpdesk</div>
+        <div
+          className="login-button"
+          onClick={() =>
+            (window.location.href = `${process.env.REACT_APP_BACKEND_URL}/auth/twitter`)
+          }
+        >
+          Sign in with Twitter
+        </div>
       </div>
     );
   }
